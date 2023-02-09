87b2eb1430c353a1369ddf56c725b67a9397d54c
Vasya Dmitrov <vd-boy400@yandex.ru>
2023-02-09 13:06:55 +0300

    итоговое ревью после исправлений

---
+++ script/index.js
@@ -0,0 +1,32 @@
+/* открыть и закрыть popup */
+const openPopup = document.querySelector('.profile__edit');
+const popup = document.querySelector('.popup');
+
+const handleOpenPopupClick = () => {
+  popup.classList.add('popup_opened');
+  nameInput.value = profileInfoName.textContent;
+  jobInput.value = profilIinfoText.textContent;
+};
+openPopup.addEventListener('click', handleOpenPopupClick);
+
+const closePopup = document.querySelector('.popup__close');
+const handleClosePopupClick = () => {
+  popup.classList.remove('popup_opened');
+};
+closePopup.addEventListener('click', handleClosePopupClick);
+
+/* изменить данные и сохранить в  popup */
+
+const formElement = popup.querySelector('.popup__form');
+const nameInput = popup.querySelector('.popup__input_type_name');
+const jobInput = popup.querySelector('.popup__input_type_info');
+const profileInfoName = document.querySelector('.profile__info-name ');
+const profilIinfoText = document.querySelector('.profile__info-text ');
+function handleFormSubmit(evt) {
+  evt.preventDefault();
+  handleClosePopupClick();
+  profileInfoName.textContent = nameInput.value;
+  profilIinfoText.textContent = jobInput.value;
+}
+
+formElement.addEventListener('submit', handleFormSubmit);
